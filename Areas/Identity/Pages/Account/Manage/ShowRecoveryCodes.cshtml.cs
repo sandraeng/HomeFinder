67264c0ddd8a30563b784b7bc2055162be69a6ed
Jonas <jonasstenbeerg@gmail.com>
2022-03-17 14:55:24 +0100

    Facebook auth added 1.6

--- Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HomeFinder.Areas.Identity.Data;
+using HomeFinder.Areas.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
