03e74c72c0de6e584acf7e29a9ed40eb78d8541e
mikosken <89968969+mikosken@users.noreply.github.com>
2022-03-20 21:59:25 +0100

    fix: Added HomeFinderContextFactory to fix error: "Unable to resolve service for type Microsoft EntityFrameworkCore DbContext"

---
+++ Data/HomeFinderContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace HomeFinder.Data
+{
+    public class HomeFinderContextFactory : IDesignTimeDbContextFactory<HomeFinderContext>
+    {
+        public HomeFinderContext CreateDbContext(string[] args)
+        {
+            // Build config
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), ".."))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HomeFinderContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("HomeFinderContextConnection"));
+            return new HomeFinderContext(optionsBuilder.Options);
+        }
+    }
+}
