9b17c1669b6f864719a4088a1afc2c0686e2f166
Jonas <jonasstenbeerg@gmail.com>
2022-03-23 15:45:25 +0100

    Added final stlyeing to cards in savedObjects page

--- Controllers/ObjectsController.cs
+++ Controllers/ObjectsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,16 +25,21 @@ namespace HomeFinder.Controllers
         }
 
 
-
         [Authorize]
-        public async Task<IActionResult> IndexUserSavedObjects()
-
+        public async Task<IActionResult> Index()
         {
             var user = await userManager.GetUserAsync(User);
 
+            //Random r = new Random();
+            //string[] Cities = new string[10] { "Skövde", "Stockholm","Malmö","Sandviken","Ystad","Trelleborg","Jönköping","Landskrona","Umeå","Gävle"};
+            //string[] Adress = new string[10] { "Miitivägen 1", "Västerlånggatan 45", "Yxgatan 1", "Ulvsundavägen 6", "Peter Bomans gata 8", "Ingen gata 1", "Brovägen 5", "Vasagatan 33", "Kungsgatan 1", "Götgatan 45" };
+            
+
+           
+
             //NoticeOfInterest noticeOfInterest = new()
             //{
-            //    PropertyObject = new PropertyObject { Address = new Address { City = "Gävle", Country = "SWEDEN", StreetAddress = "Hejsangatan 13" }, ListPrice = 6000000 },
+            //    PropertyObject = new PropertyObject { Address = new Address { City = Cities[r.Next(0,9)], Country = "SWEDEN", StreetAddress = Adress[r.Next(0,9)] }, ListPrice = r.Next(5000000,99999999) },
             //    User = user
             //};
 
@@ -52,42 +58,40 @@ namespace HomeFinder.Controllers
 
             //}
 
-            //objects[0].Images.Add(new HomeFinderImages { AltText = "blabla", Url = "https://cdn.pixabay.com/photo/2017/09/07/21/35/stilt-houses-2726812_960_720.jpg" });
-            //context.NoticeOfInterests.Remove(noticeOfInterest);
+            //objects[objects.Count()-1].Images.Add(new HomeFinderImages { AltText = "blabla", Url = "https://cdn.pixabay.com/photo/2017/09/07/21/35/stilt-houses-2726812_960_720.jpg" });
+            //context.PropertyFavorited.Add( new PropertyFavoritedByUser { PropertyObject = objects[objects.Count()-1], User = user });
+            
             //context.SaveChanges();
-            //context.SaveChanges();
+
 
 
             return View(user);
         }
 
-        private List<PropertyObject> GetObjects(HomeFinderUser user)
+        public async Task<IActionResult> RemoveLikedObject(int id)
         {
-            NoticeOfInterest noticeOfInterest = new()
+            var objToRemove = await context.PropertyFavorited.FirstOrDefaultAsync(lP => lP.PropertyObject.Id == id);
+            if (objToRemove == null)
             {
-                PropertyObject = new PropertyObject { Address = new Address { City = "Gävle", Country = "SWEDEN", StreetAddress = "Hejsangatan 13" }, ListPrice = 6000000 },
-                User = user
-            };
-            
-            context.NoticeOfInterests.Add(noticeOfInterest);
-            context.SaveChanges();
-
-           var tempObjects = context.NoticeOfInterests.Where(obj => obj.User == user).Include(obj => obj.PropertyObject).ThenInclude(prop=>prop.Images).ToList();
-           
-            List<PropertyObject> objects = new List<PropertyObject>();   
-
-            foreach (var obj in tempObjects)
-            {
-
-                objects.Add(obj.PropertyObject);
-
+                return NotFound();
             }
-
-            objects[0].Images.Add(new HomeFinderImages { AltText = "blabla", Url = "https://cdn.pixabay.com/photo/2017/09/07/21/35/stilt-houses-2726812_960_720.jpg" });
-            context.NoticeOfInterests.Remove(noticeOfInterest);
+            context.PropertyFavorited.Remove(objToRemove);
             context.SaveChanges();
 
-            return objects;
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> RemoveObjectOfInterest(int id)
+        {
+            var objToRemove = await context.NoticeOfInterests.FirstOrDefaultAsync(nI => nI.PropertyObject.Id == id);
+            if (objToRemove == null)
+            {
+                return NotFound();
+            }
+            context.NoticeOfInterests.Remove(objToRemove);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
