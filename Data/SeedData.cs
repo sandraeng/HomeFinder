110bd16ecfba283ca3ef2cb2b63414349dd724d6
Jonas <jonasstenbeerg@gmail.com>
2022-03-24 17:51:02 +0100

    SeedData fix

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -235,15 +235,15 @@ namespace HomeFinder.Data
                 
                 context.NoticeOfInterests.AddRange(
                     new NoticeOfInterest { PropertyObject = propertyObjects[4], User = users[0]},
-                    new NoticeOfInterest { PropertyObject = propertyObjects[3], User = users[1]},
-                    new NoticeOfInterest { PropertyObject = propertyObjects[2], User = users[1]}
+                    new NoticeOfInterest { PropertyObject = propertyObjects[3], User = users[3]},
+                    new NoticeOfInterest { PropertyObject = propertyObjects[2], User = users[3]}
                 );
                 context.PropertyFavorited.AddRange(
                     new PropertyFavoritedByUser { PropertyObject = propertyObjects[4], User = users[0]},
                     new PropertyFavoritedByUser { PropertyObject = propertyObjects[3], User = users[0]},
                     new PropertyFavoritedByUser { PropertyObject = propertyObjects[0], User = users[0]},
-                    new PropertyFavoritedByUser { PropertyObject = propertyObjects[5], User = users[1]},
-                    new PropertyFavoritedByUser { PropertyObject = propertyObjects[2], User = users[1]}
+                    new PropertyFavoritedByUser { PropertyObject = propertyObjects[5], User = users[3]},
+                    new PropertyFavoritedByUser { PropertyObject = propertyObjects[2], User = users[3]}
                 );
 
 
