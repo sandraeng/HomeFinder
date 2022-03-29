dabcf42db8ebd6976fd30eeb247ea52e4a2907e6
didriknilsson <didrik.nilsson@iths.se>
2022-03-29 15:10:34 +0200

    search fungerar med undantag på några parameterar

--- Controllers/MainPageController.cs
+++ Controllers/MainPageController.cs
@@ -1,6 +1,7 @@
 ﻿using HomeFinder.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace HomeFinder.Controllers
@@ -13,14 +14,43 @@ namespace HomeFinder.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString, string minNumRooms, string maxNumRooms, string minPrice, string maxPrice)
         {
             var propertyobjects = _context.PropertyObjects
                 .Include(p => p.Address)
                 .Include(p => p.PropertyType)
-                .Include(p => p.Images);
-                
-                
+                .Include(p => p.Images)
+                .Select(p => p);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                propertyobjects = propertyobjects.Where(p => p.Address.City.Contains(searchString) || p.Address.StreetAddress.Contains(searchString));
+            }
+            if (!string.IsNullOrEmpty(minNumRooms))
+            {
+                var min = int.Parse(minNumRooms);
+                propertyobjects = propertyobjects.Where(p => p.NumberOfRooms >= min);
+
+            }
+            if (!string.IsNullOrEmpty(maxNumRooms))
+            {
+                var max = int.Parse(maxNumRooms);
+                propertyobjects = propertyobjects.Where(p => p.NumberOfRooms <= max);
+
+            }
+            if (!string.IsNullOrEmpty(minPrice))
+            {
+                var min = int.Parse(minPrice);
+                propertyobjects = propertyobjects.Where(p => p.ListPrice >= min);
+
+            }
+            if (!string.IsNullOrEmpty(maxPrice))
+            {
+                var max = int.Parse(maxPrice);
+                propertyobjects = propertyobjects.Where(p => p.ListPrice <= max);
+
+            }
+
             return View(await propertyobjects.ToListAsync());
         }
         // GET: PropertyObjects/Details/5
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+var inputLeft = document.getElementById("input-left");
+var inputRight = document.getElementById("input-right"); var minimumValue = document.getElementById("minimum-value");
+var maximumValue = document.getElementById("maximum-value"); var thumbLeft = document.querySelector(".slider > .thumb.left");
+var thumbRight = document.querySelector(".slider > .thumb.right");
+var range = document.querySelector(".slider > .range"); function setLeftValue() {
+    var _this = inputLeft,
+        min = parseInt(_this.min),
+        max = parseInt(_this.max);
+    _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1); var percent = ((_this.value - min) / (max - min)) * 100; thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
+    minimumValue.value = _this.value;
+}
+setLeftValue(); function setRightValue() {
+    var _this = inputRight,
+        min = parseInt(_this.min),
+        max = parseInt(_this.max); _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1); var percent = ((_this.value - min) / (max - min)) * 100; thumbRight.style.right = (100 - percent) + "%";
+    range.style.right = (100 - percent) + "%";
+    maximumValue.value = _this.value;
+}
+setRightValue(); inputLeft.addEventListener("input", setLeftValue);
+inputRight.addEventListener("input", setRightValue);
 
-// Write your JavaScript code.
+
