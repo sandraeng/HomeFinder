04b427aa649d5344f3d283f9edf99242abc73c94
Jonas <jonasstenbeerg@gmail.com>
2022-03-31 13:53:08 +0200

    updated js to fully support range numbers

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,7 +6,8 @@ var maximumValue = document.getElementById("maximum-value");
 var thumbLeft = document.querySelector(".slider > .thumb.left");
 var thumbRight = document.querySelector(".slider > .thumb.right");
 var range = document.querySelector(".slider > .range");
-var temp1 = -1;
+var tempMin = -1;
+var tempMax = -1;
 
 function setLeftValue() {
     var _this = inputLeft,
@@ -19,14 +20,13 @@ function setLeftValue() {
     var percent = ((_this.value - min) / (max - min)) * 100;
     thumbLeft.style.left = percent + "%";
     range.style.left = percent + "%";
-    if (temp1 > 0) {
+
+    if (tempMin > 0) {
         minimumValue.value = _this.value;
-        console.log('inside first');
     }
 
     if (minimumValue.value < _this.min) {
-        temp1 = _this.value;
-        console.log('inside second');
+        tempMin = _this.value;
     }
     else {
         minimumValue.value = _this.value;
@@ -42,7 +42,16 @@ setLeftValue(); function setRightValue() {
 
     thumbRight.style.right = (100 - percent) + "%";
     range.style.right = (100 - percent) + "%";
-    maximumValue.value = _this.value;
+    if (tempMax > 0) {
+        maximumValue.value = _this.value;
+    }
+
+    if (max > _this.min) {
+        tempMax = _this.value;
+    }
+    else {
+        maximumValue.value = _this.value;
+    }
 }
 setRightValue();
 
@@ -57,7 +66,8 @@ var maximumValue2 = document.getElementById("maximum-value2");
 var thumbLeft2 = document.querySelector(".slider > .thumb.left2");
 var thumbRight2 = document.querySelector(".slider > .thumb.right2");
 var range2 = document.querySelector(".slider > .range2");
-var temp2 = -1;
+var tempMin2 = -1;
+var tempMax2 = -1;
 
 function setLeft2Value() {
     var _this = inputLeft2,
@@ -71,16 +81,16 @@ function setLeft2Value() {
     thumbLeft2.style.left = percent + "%";
     range2.style.left = percent + "%";
 
-    if (temp2 > 0)
+    if (tempMin2 > 0)
     {
         minimumValue2.value = _this.value;
-        console.log('inside first');
+        
     }
 
     if (minimumValue2.value < _this.min)
     {
-        temp2 = _this.value;
-        console.log('inside second');
+        tempMin2 = _this.value;
+        
     }
     else
     {
@@ -102,7 +112,16 @@ function setRight2Value() {
 
     thumbRight2.style.right = (100 - percent) + "%";
     range2.style.right = (100 - percent) + "%";
-    maximumValue2.value = _this.value;
+    if (tempMax2 > 0) {
+        maximumValue2.value = _this.value;
+    }
+
+    if (max > _this.min) {
+        tempMax2 = _this.value;
+    }
+    else {
+        maximumValue2.value = _this.value;
+    }
 }
 setRight2Value();
 inputLeft2.addEventListener("input", setLeft2Value);
