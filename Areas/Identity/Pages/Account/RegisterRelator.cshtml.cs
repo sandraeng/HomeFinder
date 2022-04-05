14efec8eb633ded9ec84172b06a2feed18958705
Jonas <jonasstenbeerg@gmail.com>
2022-04-05 17:30:14 +0200

    added file saveing to specific folder

--- Areas/Identity/Pages/Account/RegisterRelator.cshtml.cs
+++ Areas/Identity/Pages/Account/RegisterRelator.cshtml.cs
@@ -1,12 +1,15 @@
 using HomeFinder.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,17 +20,21 @@ namespace HomeFinder.Areas.Identity.Pages.Account
         private readonly SignInManager<HomeFinderUser> _signInManager;
         private readonly UserManager<HomeFinderUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
         //private readonly IEmailSender _emailSender;
 
         public RegisterRelatorModel(
             UserManager<HomeFinderUser> userManager,
             SignInManager<HomeFinderUser> signInManager,
-            ILogger<RegisterModel> logger
-            /*IEmailSender emailSender*/)
+            ILogger<RegisterModel> logger,
+            /*IEmailSender emailSender*/
+            IWebHostEnvironment hostingEnvironment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
             //_emailSender = emailSender;
         }
 
@@ -71,7 +78,8 @@ namespace HomeFinder.Areas.Identity.Pages.Account
             public string PhoneNumber { get; set; }
 
             public Company Company { get; set; }
-            
+
+            [Display(Name = "*Relator proof")]
             [Required]
             public IFormFile RelatorProof { get; set; }
         }
@@ -81,5 +89,69 @@ namespace HomeFinder.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                
+
+                var user = new HomeFinderUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    PhoneNumber = Input.PhoneNumber,
+                    
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    string uniqueFilename = null;
+                    if (Input.RelatorProof != null)
+                    {
+                        string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "relatorProof");
+                        uniqueFilename = Guid.NewGuid().ToString() + $"_{Input.FirstName}_{Input.LastName}_" + Input.RelatorProof.FileName;
+                        string filePath = Path.Combine(uploadsFolder, uniqueFilename);
+
+                        Input.RelatorProof.CopyTo(new FileStream(filePath, FileMode.Create));
+                    }
+
+                    _logger.LogInformation("User created a new account with password.");
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+
     }
 }
---
+++ Data/LeaseTypesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HomeFinder.Data;
+using HomeFinder.Models;
+
+namespace HomeFinder.Controllers
+{
+    public class LeaseTypesController : Controller
+    {
+        private readonly HomeFinderContext _context;
+
+        public LeaseTypesController(HomeFinderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LeaseTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LeaseTypes.ToListAsync());
+        }
+
+        // GET: LeaseTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var leaseType = await _context.LeaseTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (leaseType == null)
+            {
+                return NotFound();
+            }
+
+            return View(leaseType);
+        }
+
+        // GET: LeaseTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaseTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] LeaseType leaseType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(leaseType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(leaseType);
+        }
+
+        // GET: LeaseTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var leaseType = await _context.LeaseTypes.FindAsync(id);
+            if (leaseType == null)
+            {
+                return NotFound();
+            }
+            return View(leaseType);
+        }
+
+        // POST: LeaseTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] LeaseType leaseType)
+        {
+            if (id != leaseType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(leaseType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LeaseTypeExists(leaseType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(leaseType);
+        }
+
+        // GET: LeaseTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var leaseType = await _context.LeaseTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (leaseType == null)
+            {
+                return NotFound();
+            }
+
+            return View(leaseType);
+        }
+
+        // POST: LeaseTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var leaseType = await _context.LeaseTypes.FindAsync(id);
+            _context.LeaseTypes.Remove(leaseType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LeaseTypeExists(int id)
+        {
+            return _context.LeaseTypes.Any(e => e.Id == id);
+        }
+    }
+}
