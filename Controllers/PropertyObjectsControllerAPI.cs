dbf8e815a2e907723a92cece38121e1aa9f3048d
AmandaCarlen <90029922+AmandaCarlen@users.noreply.github.com>
2022-04-08 14:18:35 +0200

    get by id

---
+++ Controllers/PropertyObjectsControllerAPI.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HomeFinder.Data;
+using HomeFinder.Models;
+using Microsoft.Extensions.Configuration;
+using System.Web;
+
+namespace HomeFinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyObjectsControllerAPI : ControllerBase
+    {
+        private readonly HomeFinderContext _context;
+        private readonly IConfiguration configuration;
+
+        public PropertyObjectsControllerAPI(HomeFinderContext context, IConfiguration configuration)
+        {
+            _context = context;
+            this.configuration = configuration;
+        }
+
+        // GET: api/PropertyObjectsControllerAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PropertyObject>>> GetPropertyObjects()
+        {
+            return await _context.PropertyObjects.ToListAsync();
+        }
+
+        // GET: api/PropertyObjectsControllerAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PropertyObjectDTO>> GetPropertyObject(int id)
+        {
+            var propertyObject = await _context.PropertyObjects
+                .Include(p => p.Address)
+                .Include(p => p.Realtor)
+                .Include(p => p.Images)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            string apiKey = configuration.GetValue<string>("GoogleMapsAPIKey");
+            var propertyObjectDTO = new PropertyObjectDTO
+            {
+                Id = propertyObject.Id,
+                AddressId = propertyObject.AddressId,
+                PropertyTypeId = propertyObject.PropertyTypeId,
+                RealtorId = propertyObject.RealtorId,
+                Status = propertyObject.Status,
+                ListPrice = propertyObject.ListPrice,
+                NumberOfRooms = propertyObject.NumberOfRooms,
+                Area = propertyObject.Area,
+                NonLivingArea = propertyObject.NonLivingArea,
+                LotArea = propertyObject.LotArea,
+                UploadedDate = propertyObject.UploadedDate,
+                NextShowingDateTime = propertyObject.NextShowingDateTime,
+                Images = propertyObject.Images,
+                Description = propertyObject.Description,
+                LeaseTypeId = propertyObject.LeaseTypeId,
+                YearBuilt = propertyObject.YearBuilt,
+                GoogleMapsURL = GoogleMapsURL(apiKey, propertyObject.Address.FullAddress)
+
+            };
+            if (propertyObject == null)
+            {
+                return NotFound();
+            }
+
+
+            return propertyObjectDTO;
+        }
+
+        // PUT: api/PropertyObjectsControllerAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPropertyObject(int id, PropertyObject propertyObject)
+        {
+            if (id != propertyObject.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(propertyObject).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PropertyObjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PropertyObjectsControllerAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PropertyObject>> PostPropertyObject(PropertyObject propertyObject)
+        {
+            _context.PropertyObjects.Add(propertyObject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPropertyObject", new { id = propertyObject.Id }, propertyObject);
+        }
+
+        // DELETE: api/PropertyObjectsControllerAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePropertyObject(int id)
+        {
+            var propertyObject = await _context.PropertyObjects.FindAsync(id);
+            if (propertyObject == null)
+            {
+                return NotFound();
+            }
+
+            _context.PropertyObjects.Remove(propertyObject);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PropertyObjectExists(int id)
+        {
+            return _context.PropertyObjects.Any(e => e.Id == id);
+        }
+        private string GoogleMapsURL(string apiKey, string searchQuery)
+        {
+            // URL-encode search Query.
+            string q = HttpUtility.UrlPathEncode(searchQuery);
+            string src = $"https://www.google.com/maps/embed/v1/place?key={apiKey}&q={q}";
+
+            return src;
+        }
+    }
+}
---
+++ Models/PropertyObjectDTO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeFinder.Models
+{
+    public class PropertyObjectDTO
+    {
+        public int Id { get; set; }
+
+        public int PropertyTypeId { get; set; }
+
+    
+        public PropertyType PropertyType { get; set; }
+
+        public string RealtorId { get; set; }
+
+        public HomeFinderUser Realtor { get; set; }
+
+        public PropertyObjectStatus Status { get; set; }
+
+        public decimal ListPrice { get; set; }
+
+  
+        public decimal NumberOfRooms { get; set; }
+
+        public decimal Area { get; set; } // Net internal area/useable floor area. Svenska: boyta
+ 
+        public decimal NonLivingArea { get; set; } // Gross internal area minus Net internal area. Svenska: biyta
+
+        public decimal LotArea { get; set; } // Tomtyta
+
+      
+        public DateTime UploadedDate { get; set; }
+
+        public DateTime NextShowingDateTime { get; set; }
+
+        public int AddressId { get; set; }
+
+        public Address Address { get; set; }
+
+        public List<HomeFinderImages> Images { get; set; } = new List<HomeFinderImages>();
+
+        public string Description { get; set; }
+
+        public int LeaseTypeId { get; set; }
+  
+        public LeaseType LeaseType { get; set; }
+
+        public int YearBuilt { get; set; }
+
+        public string GoogleMapsURL { get; set; }
+    }
+}
