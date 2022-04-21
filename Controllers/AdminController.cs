1f22d840954e214dbcb796e986cb7cfcfac78f19
mikosken <89968969+mikosken@users.noreply.github.com>
2022-04-21 17:03:52 +0200

    feat: Added admin view to list unverified realtors. feat: Added admin view to verify UnverifiedRealtors. feat: Added admin view to delete user.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,11 +1,17 @@
-﻿using HomeFinder.Models;
+﻿using HomeFinder.Data;
+using HomeFinder.Models;
 using HomeFinder.RoleModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using MimeDetective;
+using System;
 
 namespace HomeFinder.Controllers
 {
@@ -14,11 +20,19 @@ namespace HomeFinder.Controllers
     {
         private readonly RoleManager<IdentityRole> roleManager;
         private readonly UserManager<HomeFinderUser> userManager;
+        private readonly HomeFinderContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<HomeFinderUser> userManager)
+        public AdminController(
+            RoleManager<IdentityRole> roleManager,
+            UserManager<HomeFinderUser> userManager,
+            HomeFinderContext context,
+            IWebHostEnvironment hostingEnvironment)
         {
             this.roleManager = roleManager;
             this.userManager = userManager;
+            this._context = context;
+            this._hostingEnvironment = hostingEnvironment;
         }
 
 
@@ -36,8 +50,7 @@ namespace HomeFinder.Controllers
 
             if (user == null)
             {
-                ViewBag.ErrorMessage = $"User with id: {id} cannot be found";
-                return View("Error");
+                throw new ArgumentException($"User with id: {id} could not be found.");
             }
 
             var userClaims = await userManager.GetClaimsAsync(user);
@@ -103,8 +116,7 @@ namespace HomeFinder.Controllers
 
             if(role == null)
             {
-                ViewBag.ErrorMessage = $"Role with id: {id} cannot be found";
-                return View("Error");
+                throw new ArgumentException($"Role with id: {id} could not be found.");
             }
 
             var model = new EditRole
@@ -131,8 +143,7 @@ namespace HomeFinder.Controllers
 
             if (role == null)
             {
-                ViewBag.ErrorMessage = $"Role with id: {model.Id} cannot be found";
-                return View("Error");
+                throw new ArgumentException($"Role with id: {model.Id} could not be found.");
             }
             else
             {
@@ -162,8 +173,7 @@ namespace HomeFinder.Controllers
 
             if(role == null)
             {
-                ViewBag.ErrorMessage = $"Role with id: {roleId} cannot be found";
-                return View("Error");
+                throw new ArgumentException($"Role with id: {roleId} could not be found.");
             }
 
             var model = new List<UserRole>();
@@ -196,8 +206,7 @@ namespace HomeFinder.Controllers
 
             if (role == null)
             {
-                ViewBag.ErrorMessage = $"Role with id: {roleId} cannot be found";
-                return View("Error");
+                throw new ArgumentException($"Role with id: {roleId} could not be found.");
             }
 
             for (int i = 0; i < model.Count; i++)
@@ -233,16 +242,228 @@ namespace HomeFinder.Controllers
 
         }
 
+        // GET: Admin/DeleteUser/id
+        [HttpGet]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _context.Users
+                .Where(u => u.Id == id)
+                .Include(u => u.Address)
+                .Include(u => u.Company)
+                .FirstOrDefaultAsync();
+
+            if (user is null)
+            {
+                throw new ArgumentException($"User with id: {id} could not be found.");
+            }
+
+            // Get list of files for this user. Filename contains user id.
+            string proofFolder = "realtorProof";
+            string webPath = $"/{proofFolder}";
+            string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, proofFolder);
+            List<string> fileUrls = new List<string>();
+            List<string> imageUrls = new List<string>();
+
+            DirectoryInfo di = new DirectoryInfo(uploadsFolder);
+
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                if (fi.Name.Contains(user.Id))
+                {
+                    if (IsImageFile(fi.FullName))
+                    {
+                        imageUrls.Add($"{webPath}/{fi.Name}");
+                    }
+                    else
+                    {
+                        fileUrls.Add($"{webPath}/{fi.Name}");
+                    }
+
+                }
+
+            }
+            ViewBag.UserFiles = fileUrls;
+            ViewBag.UserImages = imageUrls;
+            return View(user);
+        }
+
+        // POST: Admin/DeleteUser
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> VerifyDeleteUser(string id)
+        {
+            var user = await _context.Users
+                .Where(u => u.Id == id)
+                .Include(u => u.Address)
+                .Include(u => u.Company)
+                .FirstOrDefaultAsync();
+            var userNoIs = await _context.NoticeOfInterests.Where(n => n.UserId == user.Id).ToListAsync();
+            var userSaved = await _context.PropertyFavorited.Where(n => n.UserId == user.Id).ToListAsync();
+
+            if (user is null)
+            {
+                throw new ArgumentException($"User with id: {id} could not be found.");
+            }
+
+            // Delete files for this user. Filename contains user id.
+            string proofFolder = "realtorProof";
+            string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, proofFolder);
+
+            DirectoryInfo di = new DirectoryInfo(uploadsFolder);
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                if (fi.Name.Contains(user.Id))
+                {
+                    System.IO.File.Delete(fi.FullName);
+                }
+            }
+
+            _context.NoticeOfInterests.RemoveRange(userNoIs);
+            _context.PropertyFavorited.RemoveRange(userSaved);
+            await _context.SaveChangesAsync();
+
+            var userRoles = await userManager.GetRolesAsync(user);
+            await userManager.RemoveFromRolesAsync(user, userRoles);
+
+            var result = await userManager.DeleteAsync(user);
+            var userId = await userManager.GetUserIdAsync(user);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Unexpected error occurred deleting user with ID '{userId}'.");
+            }
+
+            return RedirectToAction("ListUsers","Admin");
+        }
+
         // GET: Admin/UnverifiedRealtors
         [HttpGet]
         public async Task<IActionResult> UnverifiedRealtors()
         {
             var users = await userManager.GetUsersInRoleAsync("UnverifiedRealtor");
             return View(users);
+        }
 
-            //string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "realtorProof");
-            //uniqueFilename = user.Id.ToString() + $"_{Input.FirstName}_{Input.LastName}";
-            //string filePath = Path.Combine(uploadsFolder, uniqueFilename);
+        // GET: Admin/ViewUnverifiedRealtors/id
+        [HttpGet]
+        public async Task<IActionResult> ViewUnverifiedRealtor(string id)
+        {
+            var user = await _context.Users
+                .Where(u => u.Id == id)
+                .Include(u => u.Address)
+                .Include(u => u.Company)
+                .FirstOrDefaultAsync();
+            if (user is null)
+            {
+                throw new ArgumentException($"User with id: {id} could not be found.");
+            }
+            if (!(await userManager.IsInRoleAsync(user,"UnverifiedRealtor")))
+            {
+                throw new ArgumentException($"User with id: {id} is not in role 'UnverifiedRealtor'.");
+            }
+
+            // Get list of files for this user. Filename contains user id.
+            string proofFolder = "realtorProof";
+            string webPath = $"/{proofFolder}";
+            string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, proofFolder);
+            List<string> fileUrls = new List<string>();
+            List<string> imageUrls = new List<string>();
+
+            DirectoryInfo di = new DirectoryInfo(uploadsFolder);
+
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                if (fi.Name.Contains(user.Id))
+                {
+                    if (IsImageFile(fi.FullName))
+                    {
+                        imageUrls.Add($"{webPath}/{fi.Name}");
+                    } else
+                    {
+                        fileUrls.Add($"{webPath}/{fi.Name}");
+                    }
+
+                }
+
+            }
+            ViewBag.UserFiles = fileUrls;
+            ViewBag.UserImages = imageUrls;
+            return View(user);
+        }
+
+        // Post: Admin/VerifyRealtor
+        [HttpPost]
+        public async Task<IActionResult> VerifyRealtor(string id)
+        {
+            var user = await _context.Users
+                .Where(u => u.Id == id)
+                .Include(u => u.Address)
+                .Include(u => u.Company)
+                .FirstOrDefaultAsync();
+            if (user is null)
+            {
+                throw new ArgumentException($"User with id: {id} could not be found.");
+            }
+            if (!(await userManager.IsInRoleAsync(user, "UnverifiedRealtor")))
+            {
+                throw new ArgumentException($"User with id: {id} is not in role 'UnverifiedRealtor'.");
+            }
+
+            return View(user);
+        }
+        // Post: Admin/RealtorVerified
+        [HttpPost]
+        public async Task<IActionResult> RealtorVerified(string id)
+        {
+            var user = await _context.Users
+                .Where(u => u.Id == id)
+                .Include(u => u.Address)
+                .Include(u => u.Company)
+                .FirstOrDefaultAsync();
+            if (user is null)
+            {
+                throw new ArgumentException($"User with id: {id} could not be found.");
+            }
+            if (!(await userManager.IsInRoleAsync(user, "UnverifiedRealtor")))
+            {
+                throw new ArgumentException($"User with id: {id} is not in role 'UnverifiedRealtor'.");
+            }
+
+            await userManager.AddToRoleAsync(user, "Realtor");
+            await userManager.RemoveFromRoleAsync(user, "UnverifiedRealtor");
+            user.EmailConfirmed = true;
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("UnverifiedRealtors","Admin");
+        }
+        /// <summary>
+        /// Returns true if file exists and has mimetype image.
+        /// Returns false otherwise.
+        /// Throws ArgumentException if filePath is null.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        private bool IsImageFile(string filePath)
+        {
+            if (filePath is null)
+            {
+                throw new ArgumentException($"filePath must be a non null string");
+            }
+            if (System.IO.File.Exists(filePath))
+            {
+                // This could be optimized if checking IsImageFile many times in a row.
+                var Inspector = new ContentInspectorBuilder()
+                {
+                    Definitions = MimeDetective.Definitions.Default.All()
+                }.Build();
+
+                var content = ContentReader.Default.ReadFromFile(filePath);
+                var results = Inspector.Inspect(content);
+                var isImage = results.ByMimeType().Any(r => r.MimeType.Contains("image/"));
+
+                return isImage;
+            }
+            return false;
         }
     }
 }
